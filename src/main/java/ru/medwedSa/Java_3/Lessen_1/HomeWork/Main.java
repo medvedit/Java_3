98dcbd86de441326d81b1b6b5a1e54ab5d3ed2a1
medvedit <113551136+medvedit@users.noreply.github.com>
2023-04-12 22:44:59 +0300

    Создал дерево проекта для обучения.

---
+++ src/main/java/ru/medwedSa/Java_3/Lessen_1/HomeWork/Main.java
@@ -0,0 +1,7 @@
+package ru.medwedSa.Java_3.Lessen_1.HomeWork;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("...");
+    }
+}
---
+++ src/main/java/ru/medwedSa/Java_3/Lessen_2/HomeWork/Main.java
@@ -0,0 +1,7 @@
+package ru.medwedSa.Java_3.Lessen_2.HomeWork;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("...");
+    }
+}
