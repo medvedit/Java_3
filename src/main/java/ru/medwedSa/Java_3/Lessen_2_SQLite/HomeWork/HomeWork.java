b354bd2878673dd75db9af03f06b4b7aae36b18f
medvedit <113551136+medvedit@users.noreply.github.com>
2023-04-27 15:25:22 +0300

    Домашка....

---
+++ src/main/java/ru/medwedSa/Java_3/Lessen_2_SQLite/HomeWork/HomeWork.java
@@ -0,0 +1,7 @@
+package ru.medwedSa.Java_3.Lessen_2_SQLite.HomeWork;
+
+public class HomeWork {
+    public static void main(String[] args) {
+        System.out.println("А тут нечего делать....");
+    }
+}
