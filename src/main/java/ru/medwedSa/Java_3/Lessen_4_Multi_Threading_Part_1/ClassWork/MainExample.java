5def9c5aeb863df7a4eb449078826ff7602df902
medvedit <113551136+medvedit@users.noreply.github.com>
2023-05-14 14:22:58 +0300

    Изменил дерево нахождения файлов

--- src/main/java/ru/medwedSa/Java_3/Lessen_4_Multi_Threading_Part_1/ClassWork/MainExample.java
+++
@@ -1,5 +0,0 @@
-package ru.medwedSa.Java_3.Lessen_4_Multi_Threading_Part_1.ClassWork;
-
-public class MainExample {
-
-}
--- src/main/java/ru/medwedSa/Java_3/Lessen_4_Multi_Threading_Part_1/ClassWork/MyltiThreadingExample_java2.java
+++ src/main/java/ru/medwedSa/Java_3/Lessen_4_Multi_Threading_Part_1/ClassWork/MyltiThreadingExample_java2.java
@@ -1,4 +1,4 @@
-package ru.medwedSa.Java_3.Lessen_4_Multi_Threading_Part_1.ClassWork.ExampleThreading;
+package ru.medwedSa.Java_3.Lessen_4_Multi_Threading_Part_1.ClassWork;
 
 
 
